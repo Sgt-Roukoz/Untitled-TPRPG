707b8566f937d77079d8d7c0fb2ddbd7b071041b
Marwan <marwan.zeid00@gmail.com>
2020-07-16 22:35:57 -0400

    Importing Model and Beginning Scripts

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private Vector3 playerVelocity;
+    private float playerSpeed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w")){
+            transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+        }
+    }
+}
