9f53164b1bf910f591f057d9990762f64f88bf9a
Marwan <marwan.zeid00@gmail.com>
2020-07-19 00:34:33 -0700

    Recommit

---
+++ Assets/Scripts/GeneralSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralSettings : MonoBehaviour
+{
+    // Settings preset by game and determined by user, will be modified as time goes on
+    void Start()
+    {
+        Cursor.visible = false; // hide cursor
+    }
+
+}
