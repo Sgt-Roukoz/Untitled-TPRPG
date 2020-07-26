75e9cf7611a9793cce59af484d7b8f62c179622d
Marwan <marwan.zeid00@gmail.com>
2020-07-25 22:19:52 -0400

    Created Basic Interaction Code
    Created Raycast object, and allowed for left click to shoot ray, can distinguish between interactable object and not.

---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public GameObject rayObject;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForHit();
+    }
+
+    void CheckForHit()
+    {
+        Vector3 fwd = rayObject.transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(rayObject.transform.position, fwd * 50, Color.green);
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if(Physics.Raycast(rayObject.transform.position, fwd, out hit, 50))
+            {
+                if(hit.collider.gameObject.name == "Item")
+                {
+                    Debug.Log("Item Hit!");
+                }
+            }
+        }
+    }
+}
